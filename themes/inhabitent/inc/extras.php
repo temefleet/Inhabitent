9e75475d363b121808b8f31065bd0b4218ceb2fa
jefe temeso <jefe@fleet.local>
2017-08-11 16:17:22 -0700

    Project starter files commit

---
+++ themes/inhabitent/inc/extras.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package inhabitent_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function inhabitent_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'inhabitent_body_classes' );
+
+// change login logo ------------------
+function inhabitent_login_logo() { ?>
+	<style type="text/css"> 
+    
+	#login h1 a, .login h1 a {
+    background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text-dark.svg);
+		height:53px;
+		width:300px;
+		background-size: 300px 53px;
+		background-repeat: no-repeat;
+  }
+	#login .button.button-primary {
+			background-color: #248A83;
+	}
+    </style>
+<?php }
+add_action('login_head', 'inhabitent_login_logo');
+
+
+// hover on login logo title
+function inhabitent_login_title() {
+	return 'Inhabitent';
+}
+add_filter('login_headertitle', 'inhabitent_login_title');
+
+
+// link login logo to homepage
+function home_page_login_url() {
+    return home_url();
+}
+add_filter( 'login_headertitle', 'home-page-login-url' );
