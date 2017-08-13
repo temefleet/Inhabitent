59c9cdb793afd05f89a2caaf62e3e01b4dea8b10
jefe <jefe@fleet.local>
2017-08-13 13:39:02 -0700

    Styled About Page

--- themes/inhabitent/gulpfile.js
+++ themes/inhabitent/gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('sass/*.scss', ['sass']);
     gulp.watch('./js/*.js', ['scripts']);
 });
 
