d992da252fc3f58425981406cf57d6392b57b9df
jefe <jefe@fleet.local>
2017-08-14 15:19:06 -0700

    Updated About.php

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -10,12 +10,25 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header custom-hero">
+					<div class="container">
+						<?php the_title( '<h1 class="entry-title">', '</h1>'); ?>
+						<?php echo CFS()->get( 'about_header_image'); ?>
+					</div>
+				</header>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<div class="container">
+					<div class="entry-content">
+						<h2>Our Story</h2>
+						<?php echo CFS()->get( 'about_our_story' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
-
+						<h2>Our Team</h2>
+						<?php echo CFS()->get( 'about_our_team' ); ?>
+					</div>
+				</div>
+			</article>
+		
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
