f88326568b0e629c88dc181fbef41246478123cc
jefe <jefe@fleet.local>
2017-08-13 21:01:27 -0700

    Styled beginning of Journal

--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -10,7 +10,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
+			<?php the_post_thumbnail( 'full' ); ?>
 		<?php endif; ?>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
