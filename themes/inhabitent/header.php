3bfdee4dac4ef59772d3ba8033ddda340a711c2a
jefe <jefe@fleet.local>
2017-08-29 11:06:55 -0700

    Fixed search toggle jumping to the top of the page

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -35,7 +35,7 @@
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						
-						<a href="#" class="search-toggle" aria-hidden="true">
+						<a class="search-toggle" aria-hidden="true">
 							<i class="fa fa-search"></i>
 						</a>
 		
